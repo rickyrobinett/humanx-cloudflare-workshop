ba607a48301b774299bc0a76336e1fdb22704b7f
Ricky Robinett <richard.robinett@gmail.com>
2025-03-12 22:06:22 -0700

    begin working on Anthropic integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { WorkflowEntrypoint } from "cloudflare:workers";
+import Anthropic from '@anthropic-ai/sdk';
 
 const app = new Hono();
 app.get('/', async (c) => {
@@ -30,21 +31,53 @@ app.get('/', async (c) => {
 			? `Context:\n${notes.map(note => `- ${note}`).join("\n")}`
 			: ""
 
-		const systemPrompt = `When answering the question or responding, use the context provided, if it is provided and relevant.`
+			const systemPrompt = `When answering the question or responding, use the context provided, if it is provided and relevant.`
 
-
-		const { response: answer } = await c.env.AI.run(
-			'@cf/meta/llama-3-8b-instruct',
-			{
-			messages: [
-				...(notes.length ? [{ role: 'system', content: contextMessage }] : []),
-				{ role: 'system', content: systemPrompt },
-				{ role: 'user', content: question }
-			]
+			let modelUsed = ""
+			let response = null
+		  
+			if (c.env.ANTHROPIC_API_KEY) {
+			  const anthropic = new Anthropic({
+				apiKey: c.env.ANTHROPIC_API_KEY
+			  })
+		  
+			  const model = "claude-3-5-sonnet-latest"
+			  modelUsed = model
+		  
+			  const message = await anthropic.messages.create({
+				max_tokens: 1024,
+				model,
+				messages: [
+				  { role: 'user', content: question }
+				],
+				system: [systemPrompt, notes ? contextMessage : ''].join(" ")
+			  })
+		  
+			  response = {
+				response: message.content.map(content => content.text).join("\n")
+			  }
+			} else {
+			  const model = "@cf/meta/llama-3.1-8b-instruct"
+			  modelUsed = model
+		  
+			  response = await c.env.AI.run(
+				model,
+				{
+				  messages: [
+					...(notes.length ? [{ role: 'system', content: contextMessage }] : []),
+					{ role: 'system', content: systemPrompt },
+					{ role: 'user', content: question }
+				  ]
+				}
+			  )
 			}
-		)
-
-		return c.text(answer);
+		  
+			if (response) {
+			  c.header('x-model-used', modelUsed)
+			  return c.text(response.response)
+			} else {
+			  return c.text("We were unable to generate output", 500)
+			}
 	} catch (error) {
 		return c.text(error.message, 500);
 	}
